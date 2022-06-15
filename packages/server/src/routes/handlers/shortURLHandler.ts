780855c2adf3392ed220634ba3af27649f7c177a
Adeline Gayle Noman <nomanal@ph.ibm.com>
2022-06-16 05:14:59 +0800

    fix formatting

--- packages/server/src/routes/handlers/shortURLHandler.ts
+++ packages/server/src/routes/handlers/shortURLHandler.ts
@@ -1,24 +1,21 @@
 const ShortUniqueId = require("short-unique-id");
 const Url = require("../../model/Url");
 
-const createTinyUrl = async(req: any, res: any) => {
-  const {longURL} = req.body;
+const createTinyUrl = async (req: any, res: any) => {
+  const { longURL } = req.body;
   const shortID = new ShortUniqueId({ length: 6 })();
 
   const urlData = new Url({
     shortURL: shortID,
     longURL,
-    userID: ''
+    userID: "",
   });
-try{
-  await urlData.save();
-  res.status(200).json({ shortURL: shortID });
-}catch(err: any) {
-  res.status(400).json({error: err.message});
-}
-  
-
-  
+  try {
+    await urlData.save();
+    res.status(200).json({ shortURL: shortID });
+  } catch (err: any) {
+    res.status(400).json({ error: err.message });
+  }
 };
 
 module.exports = {
